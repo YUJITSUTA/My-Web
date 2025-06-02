c7bcbd601ddcb1089fc92e421693e181077745cd
YUJITSUTA <yujitsuta20@gmail.com>
2025-06-02 23:35:58 +0800

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -1,10 +1,16 @@
 const myname = 'YUJI B. TSUTA';
+const intromsg = 'Hi, \n this is '+myname+' \n Software Engineer';
 const myemail = 'yujitsuta20@gmail.com';
 const mycontact = '(962) 251 7473';
 const myaddress = 'Mississippi St., Purok 3, Brgy. Malanday, Marikina City';
 const careersum = 'Software developer focused on building practical and user-friendly web applications. Experienced with JavaScript, Python, and modern web frameworks. Background in interior design provides a strong sense of detail and visual organization, which translates well into web development. Continuously learning and committed to writing clean, effective code that solves real-world problems.';
 const myfbpageURL = 'https://www.facebook.com/atust20/';
 const mylinkedinURL = 'https://www.linkedin.com/in/yujitsuta'
+
+// Set the text content
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("intromsg-text").textContent = intromsg;
+  });
 
 function openemailURL() {
     const autoemail = 'mailto:'+myemail;
