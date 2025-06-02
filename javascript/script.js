ea82b73e8e06aa7a01aa886826ed91d407e90790
YUJITSUTA <yujitsuta20@gmail.com>
2025-06-02 17:06:39 +0800

    Add files via upload

---
+++ javascript/script.js
@@ -0,0 +1,20 @@
+const myname = 'YUJI B. TSUTA';
+const myemail = 'yujitsuta20@gmail.com';
+const mycontact = '(962) 251 7473';
+const myaddress = 'Mississippi St., Purok 3, Brgy. Malanday, Marikina City';
+const careersum = 'Software developer focused on building practical and user-friendly web applications. Experienced with JavaScript, Python, and modern web frameworks. Background in interior design provides a strong sense of detail and visual organization, which translates well into web development. Continuously learning and committed to writing clean, effective code that solves real-world problems.';
+const myfbpageURL = 'https://www.facebook.com/atust20/';
+const mylinkedinURL = 'https://www.linkedin.com/in/yujitsuta'
+
+function openemailURL() {
+    const autoemail = 'mailto:'+myemail;
+    window.open(autoemail, '_blank');
+}
+
+function openfbURL() {
+    window.open(myfbpageURL, '_blank');
+}
+
+function openlinkedinURL() {
+    window.open(mylinkedinURL, '_blank');
+}
