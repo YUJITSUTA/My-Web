82d45eb83416ae6ded4213a9d82169cac85c3162
YUJITSUTA <yujitsuta20@gmail.com>
2025-06-25 09:17:39 +0800

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -1,15 +1,22 @@
-const myname = 'YUJI B. TSUTA';
-const intromsg = 'Hi, \n this is '+myname+' \n Software Engineer';
+const myname = 'YUJI TSUTA';
+const mygreet = 'Hi!';
+const mygreet2 = "I'm";
+const intromsg = "Hi! \n I'm "+myname.trim().split(" ")[0]+" \n Software Engineer";
 const myemail = 'yujitsuta20@gmail.com';
 const mycontact = '(962) 251 7473';
 const myaddress = 'Mississippi St., Purok 3, Brgy. Malanday, Marikina City';
-const careersum = 'Software developer focused on building practical and user-friendly web applications. Experienced with JavaScript, Python, and modern web frameworks. Background in interior design provides a strong sense of detail and visual organization, which translates well into web development. Continuously learning and committed to writing clean, effective code that solves real-world problems.';
+const careersum = 'A Software Developer and Computer-Aided Designer, Experienced with JavaScript, Python, and 3D modeling softwares such as SketchUP, and Kujiale or Coohom for interactive 360 walktrough. Studied Computer Engineering and have a background in Interior Design that provides a strong sense of detail and visual. Continuously learning and committed programming and design.';
 const myfbpageURL = 'https://www.facebook.com/atust20/';
 const mylinkedinURL = 'https://www.linkedin.com/in/yujitsuta'
 
+
+
 // Set the text content
 document.addEventListener("DOMContentLoaded", () => {
-    document.getElementById("intromsg-text").textContent = intromsg;
+    document.getElementById("mygreet-text").textContent = mygreet;
+    document.getElementById("mygreet2-text").textContent = mygreet2;
+    document.getElementById("myname-text").textContent = myname;
+    document.getElementById("careersum-text").textContent = careersum;
   });
 
 function openemailURL() {
